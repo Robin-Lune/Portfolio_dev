cb1d970a30fa9153978d87260bf1e14edab233c3
Robin LEBON <robinlebon974@gmail.com>
2022-03-27 14:31:17 +0400

    change index facebook card img

--- components/footer.js
+++ components/footer.js
@@ -5,17 +5,17 @@ const Footer = () => {
     <footer className="container">
       <div className="Box fb">
         <a href="https://www.facebook.com/robin974" target="_blank" rel="noreferrer">
-          <img src="./images/Web//N&B/facebook_N&B.png" alt="" id="Fb" />
+          <img src="./images/Web/N&B/facebook_N&B.png" alt="" id="Fb" />
         </a>
       </div>
       <div className="Box insta">
         <a href="https://www.instagram.com/robin_lune/" target="_blank" rel="noreferrer">
-          <img src="./images/Web//N&B/instagram_N&B.png" alt="" id="Insta" />
+          <img src="./images/Web/N&B/instagram_N&B.png" alt="" id="Insta" />
         </a>
       </div>
       <div className="Box twi">
         <a href="https://twitter.com/Robin_Lune" target="_blank" rel="noreferrer">
-          <img src="./images/Web//N&B/twitter_N&B.png" alt="" id="Twi" />
+          <img src="./images/Web/N&B/twitter_N&B.png" alt="" id="Twi" />
         </a>
       </div>
       <div className="Box git">
--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ const Home = () => {
          <meta property="og:title" content="Robin LEBON | Développeur" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.dev.robin-lebon.com" />
-        <meta property="og:image" content="https://www.dev.robin-lebon.com/_next/image?url=%2Fimages%2Fsite-dev.jpg&w=750&q=75" />
+        <meta property="og:image" content="https://www.dev.robin-lebon.com/_next/image?url=%2Fimages%2Fsite-dev.jpg&w=750&q=75" key="ogimage"/>
         <meta
           property="og:description"
           content="Portfolio de Robin LEBON Développeur"
@@ -48,6 +48,7 @@ const Home = () => {
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@publisher_handle" />
         <meta name="twitter:creator" content="@author_handle" />
+        <meta name="twitter:image" content="https://www.dev.robin-lebon.com/_next/image?url=%2Fimages%2Fsite-dev.jpg&w=750&q=75" />
       </Head>
 
       <section>

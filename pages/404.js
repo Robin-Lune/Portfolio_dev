6b4b22a8d80e6c8e5f1d29f04ac5ea14bb552207
Robin LEBON <robin.lebon@runnet.fr>
2022-08-30 18:30:25 +0400

    rectif apostrophe 404

--- pages/404.js
+++ pages/404.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Custom404 = () => {
     return (
         <section className='container_404'>
-            <h1 className='side_text'> NOT FOUND</h1>
+            <h1 className='side_text'>NOT FOUND</h1>
             <h2 className='error_title' data-text='404'>404</h2>
-            <p>ahahah vous n'avez pas dis le mot magique !!!</p>
+            <p>ahahah vous n&apos;avez pas dis le mot magique !!!</p>
 
         </section>
     );
